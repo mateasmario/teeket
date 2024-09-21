7422c0416d5d147ea6f7e28afa71415874e85e9d
mateasmario <mmmateas@gmail.com>
2024-09-21 08:10:16 +0300

    Created initial project structure

---
+++ src/main/java/dev/mateas/teeket/TeeketApplication.java
@@ -0,0 +1,13 @@
+package dev.mateas.teeket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeeketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeeketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mateas/teeket/TeeketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mateas.teeket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeeketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
